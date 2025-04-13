512f83ac5e911d024f80b57accc4e9d669967621
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2025-04-13 12:35:42 +0100

    created utils to add custom services

---
+++ src/utils/db.js
@@ -0,0 +1,14 @@
+export const Shopcards = [
+    {
+        id: 1,
+        title:"The Contract Shop",
+        body: "Welcome to the contract shop",
+        image: "/stew/",
+    },
+    {
+    id: 2,
+    title:"Custom Legal",
+    body: "Welcome to Custom Legal",
+    image: "/stew/",
+}
+]
