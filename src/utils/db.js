7f2b381ef4aebf05e204375e81649d3dd91f3099
Moses Sochuks Kajang <moseschukwumakajang@gmail.com>
2025-04-19 07:22:03 +0100

    Modified services section. added dynamic data

--- src/utils/db.js
+++ src/utils/db.js
@@ -3,12 +3,51 @@ export const Shopcards = [
         id: 1,
         title:"The Contract Shop",
         body: "Welcome to the contract shop",
-        image: "/stew/",
+        image: "https://img.freepik.com/free-photo/close-up-business-document-laptop-desk_1098-1991.jpg?t=st=1744559323~exp=1744562923~hmac=8951406942169f850f37cafc46bd7693d3c15f5c3eac8b3c16fec307d8b52171&w=996",
     },
     {
     id: 2,
     title:"Custom Legal",
     body: "Welcome to Custom Legal",
-    image: "/stew/",
+    image: "https://img.freepik.com/premium-photo/closeup-lawyer-signing-legal-documents-with-pen-courtroom_885831-95998.jpg?w=740",
 }
+]
+
+export const Services = [
+    {
+        "id": 1,
+        "title": "Business Formation",
+        "description": "Strategic setup for your business to launch strong and grow smart.",
+        "image": "https://img.icons8.com/sf-regular/96/business.png"
+      },
+      {
+        "id": 2,
+        "title": "Data Privacy & Internet Law",
+        "description": "Comprehensive legal support to safeguard your online presence, from data privacy compliance to internet regulations.",
+        "image": "https://img.icons8.com/sf-regular/96/data-protection.png"
+      },
+      {
+        "id": 3,
+        "title": "Tech Contracts",
+        "description": "Custom, tech-focused contracts to lock in your business deals with clarity and confidence.",
+        "image": "https://img.icons8.com/sf-regular-filled/48/agreement.png"
+      },
+      {
+        "id": 4,
+        "title": "Cyber Security Legal Support",
+        "description": "Strategic setup for your business to launch strong and grow smart.",
+        "image": "https://img.icons8.com/sf-regular/96/security-shield-green.png"
+      },
+      {
+        "id": 5,
+        "title": "Copyright Protection",
+        "description": "Comprehensive legal support to safeguard your online presence, from data privacy compliance to internet regulations.",
+        "image": "https://img.icons8.com/ios-filled/50/copyright.png"
+      },
+      {
+        "id": 6,
+        "title": "Tech Contracts",
+        "description": "Custom, tech-focused contracts to lock in your business deals with clarity and confidence.",
+        "image": "https://img.icons8.com/sf-regular/96/commercial.png"
+      }
 ]
